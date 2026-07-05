970cd48f62d486878917aa51757524530d9dc108
Diego Valentin Lozano <diegovalentinlozano@192.168.1.14>
2026-07-05 15:45:24 -0300

    feat: login y register con Google Auth + servicios y modelos

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,25 +1,44 @@
-import { Component, inject } from '@angular/core';
+declare var google: any;
+
+import { Component, AfterViewInit, OnDestroy, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../../../core/services/auth';
+import { GoogleAuthService, GoogleUser } from '../../../core/services/google-auth';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements AfterViewInit, OnDestroy {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router);
+  private googleAuthService = inject(GoogleAuthService);
+  private userSub!: Subscription;
 
   loading = false;
+  googleUser: GoogleUser | null = null;
 
   form = this.fb.nonNullable.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
+
+  ngAfterViewInit(): void {
+    this.googleAuthService.initializeGoogleSignIn('googleBtn');
+    this.userSub = this.googleAuthService.currentUser$.subscribe((user) => {
+      this.googleUser = user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub?.unsubscribe();
+  }
 
   onSubmit(): void {
     if (this.form.invalid) {
@@ -32,5 +51,9 @@ export class Login {
       this.loading = false;
       this.router.navigateByUrl('/');
     });
+  }
+
+  signOut(): void {
+    this.googleAuthService.signOut();
   }
 }
