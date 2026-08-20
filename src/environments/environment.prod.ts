28bb8bcac168f9df452a3a47c6ec43da7f2b2f2f
Martin Mamani <martinnnxz@gmail.com>
2026-08-20 19:24:00 -0300

    chore: update production apiUrl with render url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Cambiaremos esta URL por la de Render cuando hagamos el deploy
-  apiUrl: 'https://gymhub-api.onrender.com/api' 
+  apiUrl: 'https://gymhub-api-vbs1.onrender.com/api' 
 };
